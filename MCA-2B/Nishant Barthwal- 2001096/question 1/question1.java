2b46c1f3d4b2ab6f38cfe3f2a4b9e8ccf2962db6
Nishantbarthwal <85736217+Nishantbarthwal@users.noreply.github.com>
2021-06-22 12:16:07 +0530

    Add files via upload

---
+++ MCA-2B/Nishant Barthwal- 2001096/question 1/question1.java
@@ -0,0 +1,81 @@
+package assignment5;
+
+import java.io.*;
+public class FileMerge 
+{
+    public static void printDATA()
+{
+    BufferedReader br = null;
+    BufferedReader br1 = null;
+    BufferedReader br2 = null;
+    try {
+        br = new BufferedReader(new FileReader("one.txt"));
+        br1 = new BufferedReader(new FileReader("two.txt"));
+        br2 = new BufferedReader(new FileReader("three.txt"));
+    } catch (FileNotFoundException e) {
+        
+        e.printStackTrace();
+    }
+     String line = null;
+     String line1 = null;
+     String line2 = null;
+
+
+     try {
+        while((line = br.readLine())!= null) {
+
+            while((line1 = br1.readLine())!= null){
+            	while((line2 = br2.readLine())!= null)
+            	{
+            		System.out.println("Content of one.txt file : " + line);
+                    System.out.println("Content of two.txt file : " + line1);
+                    System.out.println("Content of three.txt file : " + line2);
+            	}
+            }
+           
+            }
+         
+    } catch (IOException e) {
+       
+        e.printStackTrace();
+    }
+}
+    public static void main(String[] args) throws IOException 
+    {
+        PrintWriter pw = new PrintWriter("three.txt");
+          
+        BufferedReader br = new BufferedReader(new FileReader("one.txt"));
+        
+          
+        String line = br.readLine();
+
+        while (line != null)
+        {
+            pw.print(line);
+            line = br.readLine();
+        }
+          
+        br = new BufferedReader(new FileReader("two.txt"));
+          
+        line = br.readLine();
+          
+        while(line != null)
+        {
+            pw.print(line);
+            line = br.readLine();
+            
+        
+          
+        pw.flush();
+          
+
+        br.close();
+        pw.close();
+        
+        System.out.println("Copied the contents of one.txt and two.txt into three.txt file");
+        System.out.println(" ");
+        }
+        printDATA(); 
+    }
+    
+}
