1f33778261c720ca68b4104938d0bcebb2c2775b
Ashish Semwal <67877297+Ashish-B2P@users.noreply.github.com>
2021-06-22 11:54:54 +0530

    Update Ques1.java

--- MCA- 2A/AshishSemwal-2001141/Q1/Ques1.java
+++ MCA- 2A/AshishSemwal-2001141/Q1/Ques1.java
@@ -1,68 +1,60 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
-public class Ques1 {
-    public static void main(String[] args) throws IOException 
-    {
-        // PrintWriter object for file3.txt
-        PrintWriter out = new PrintWriter("file1.txt");
-        out.println("1"); 
-        out.println("2"); 
-        out.println("3"); 
-        out.println("4"); 
-        out.println("5"); 
-         out.close(); 
-         
-        var out2 = new PrintWriter("file2.txt");
-        out.println("6"); 
-        out.println("7"); 
-        out.println("8"); 
-        out.println("9"); 
-        out.println("10"); 
-         out2.close(); 
-         
-   
-     PrintWriter pw = new PrintWriter("three.txt"); 
-          
-        // BufferedReader object for file1.txt
-        BufferedReader br1 = new BufferedReader(new FileReader("file1.txt"));
-        BufferedReader br2 = new BufferedReader(new FileReader("file2.txt"));
-          
-          
-        String line1 = br1.readLine();
-        String line2 = br2.readLine();
-          
-        // loop to copy lines of 
-        // file1.txt and file2.txt 
-        // to  file3.txt alternatively
-        while (line1 != null || line2 !=null)
-        {
-            if(line1 != null)
-            {
-                pw.println(line1);
-                line1 = br1.readLine();
-            }
-              
-            if(line2 != null)
-            {
-                pw.println(line2);
-                line2 = br2.readLine();
-            }
-        }
-      
-        pw.flush();
-          
-        // closing resources
-        br1.close();
-        br2.close();
-        pw.close();
-          
-        System.out.println("Merged file1.txt and file2.txt alternatively into file3.txt");
-        br1 = new BufferedReader(new FileReader("three.txt"));
+import java.io.*; 
   
-           String st;
-           while ((st = br1.readLine()) != null)
-             System.out.println(st);
-    }
+class FileMerge  
+{ 
+    public static void main(String[] args) throws IOException  
+    
+    {
+		
+		  PrintWriter a1 = new PrintWriter("one.txt");
+		   a1.println("line1"); 
+		   a1.println("line2"); 
+		   a1.println("line3"); 
+		   a1.println("line4"); 
+		   a1.println("line5"); 
+		    a1.close(); 
+		    
+		      PrintWriter a2 = new PrintWriter("two.txt");
+		   a2.println("line11"); 
+		   a2.println("line22"); 
+		   a2.println("line33"); 
+		   a2.println("line44"); 
+		   a2.println("line55"); 
+		    a2.close(); 
+		    
+      
+        PrintWriter pw = new PrintWriter("three.txt"); 
+          
+        BufferedReader br1 = new BufferedReader(new FileReader("one.txt")); 
+        BufferedReader br2 = new BufferedReader(new FileReader("two.txt")); 
+          
+          
+        String line1 = br1.readLine(); 
+        String line2 = br2.readLine(); 
+          
+       
+        while (line1 != null || line2 !=null) 
+        { 
+            if(line1 != null) 
+            { 
+                pw.println(line1); 
+                line1 = br1.readLine(); 
+            } 
+              
+            if(line2 != null) 
+            { 
+                pw.println(line2); 
+                line2 = br2.readLine(); 
+            } 
+        } 
+      
+        pw.flush(); 
+          
+     
+        br1.close(); 
+        br2.close(); 
+        pw.close(); 
+          
+        System.out.println("Merged one.txt and two.txt into three.txt"); 
+    } 
 }
